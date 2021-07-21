e4d7c1b29798952c88e29bed7f29f020aebbacfc
shreyanshu <shreyanshukunwar@gmail.com>
2021-07-21 09:32:57 +0545

    added form

---
+++ src/components/AddTask.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function AddTask() {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+  const [reminder, setReminder] = useState(false);
+
+  return (
+    <form className="add-form" action="">
+      <div className="form-control">
+        <label>Task</label>
+        <input
+          type="text"
+          name=""
+          placeholder="Add Task"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+      </div>
+      <div className="form-control">
+        <label>Day & Time</label>
+        <input
+          type="text"
+          name=""
+          placeholder="Add Day and Time"
+          value={day}
+          onChange={(e) => setDay(e.target.value)}
+        />
+      </div>
+      <div className="form-control form-control-check">
+        <label>Set Reminder</label>
+        <input
+          type="checkbox"
+          value={reminder}
+          onChange={(e) => setReminder(e.currentTarget.checked)}
+        />
+      </div>
+
+      <input className="btn btn-block" type="submit" value="Save Task" />
+    </form>
+  );
+}
+
+export default AddTask;
